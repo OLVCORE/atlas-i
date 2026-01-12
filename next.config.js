73d697621ed1acae3a5e9d6833e22e6fad48a785
Marcos Oliveira <olvsistemas@olvinternacional.com.br>
2026-01-12 11:31:32 -0300

    fix: configurar next.config.js e usar chromium-min corretamente

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium-min"],
 }
 
 module.exports = nextConfig
